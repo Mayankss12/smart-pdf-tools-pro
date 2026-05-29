61c13821e262aa2d522234de9c43012fb73faad0
Mayankss12 <33421693+Mayankss12@users.noreply.github.com>
2026-05-29 17:34:52 +0530

    Add experimental serverActions configuration

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
+  },
 };
-
 module.exports = nextConfig;
